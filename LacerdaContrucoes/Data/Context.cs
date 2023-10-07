424577619d8607e9c0b1c60c02a596392eb9b75d
Guedes <jpguedesbarbieri@gmail.com>
2023-10-07 14:52:32 -0300

    criados

--- LacerdaContrucoes/Data/Context.cs
+++ LacerdaContrucoes/Data/Context.cs
@@ -10,13 +10,18 @@ namespace LacerdaContrucoes.Data
         public DbSet<Cliente> Clientes { get; set;}
         public DbSet<Fornecedor> Fornecedores { get; set;} 
         public DbSet<CategoriaDeProdutos> CategoriaDeProdutos { get; set;}
-
+        public DbSet<Produto> Produtos { get; set;}
+        public DbSet<Venda> Venda { get; set;}
+        public DbSet<CadVendas> CadVendas { get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Cliente>().ToTable("tbClientes");
             modelBuilder.Entity<Fornecedor>().ToTable("tbFornecedores");
             modelBuilder.Entity<CategoriaDeProdutos>().ToTable("tbCategoriaDeProdutos");
+            modelBuilder.Entity<Produto>().ToTable("tbProdutos");
+            modelBuilder.Entity<Venda>().ToTable("tbVendas");
+            modelBuilder.Entity<CadVendas>().ToTable("tbCadVendas");
         }
 
     }
