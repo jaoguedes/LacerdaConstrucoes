e57177933082def6d96c1416f35b34207e01c863
Guedes <jpguedesbarbieri@gmail.com>
2023-10-10 11:04:48 -0300

    CERTO

--- LacerdaContrucoes/Models/CadVendas.cs
+++ LacerdaContrucoes/Models/CadVendas.cs
@@ -6,12 +6,15 @@ namespace LacerdaContrucoes.Models
     public class CadVendas
     {
         public Guid CadVendasId { get; set; }
-        public Guid NotaDaVenda {  get; set; }
+        public int NotaDaVenda { get; set; }
         public DateTime DataDaVenda { get; set; }
 
-        [DisplayName("Produtos")]
-        public Guid? VendaId { get; set; }
-        public Venda? Venda { get; set; }
+        [DisplayName("Cliente")]
+        public Guid? ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+
+        [DisplayName("Item da venda")]
+        public IEnumerable<Venda> Vendas { get; set; }
 
     }
 }
